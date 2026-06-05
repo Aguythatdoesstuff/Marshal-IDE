084f03afbcf17841d321b800dda0028e57419803
Aguythatdoesstuff <94752413+Aguythatdoesstuff@users.noreply.github.com>
2026-06-05 17:41:07 +0200

    Published compiler

--- c#/Compiler/Compiler/obj/Debug/net10.0/Compiler.AssemblyInfo.cs
+++ c#/Compiler/Compiler/obj/Debug/net10.0/Compiler.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Compiler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d80dcd4d33e079f0a1346375fef656062cab36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c26b389e8bfaf14179b229cd8043be083e6abd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Compiler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Compiler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
